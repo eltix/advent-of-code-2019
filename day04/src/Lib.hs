fdcee7b001355868d2c9602c577d00f65dc66cdd
etixier <etixier@mp-41205.local>
2019-12-21 15:05:48 +0100

    day 04

--- day04/src/Lib.hs
+++ day04/src/Lib.hs
@@ -4,42 +4,50 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE NamedFieldPuns #-}
 
 module Lib
   ( part1
   , part2
+  , testPrograms
   ) where
 
 import Preprocessing (readProgram)
 
 import BasicPrelude
-import Data.Maybe (listToMaybe)
+import Control.Lens
 
 type Program = [Int]
 
-data OpCode = Add | Multiply | Input | Output
-  deriving Show
+data OpCode =
+  Add | Multiply | Input | Output | JumpIfTrue | JumpIfFalse
+  | LessThan | Equals | Halt
+    deriving Show
 
 fromIntToOpCode :: Int -> OpCode
 fromIntToOpCode = \case
-  1 -> Add
-  2 -> Multiply
-  3 -> Input
-  4 -> Output
-  n -> error $ "Unknown opcode: " ++ show n
+  1  -> Add
+  2  -> Multiply
+  3  -> Input
+  4  -> Output
+  5  -> JumpIfTrue
+  6  -> JumpIfFalse
+  7  -> LessThan
+  8  -> Equals
+  99 -> Halt
+  n  -> error $ "Unknown opcode: " ++ show n
 
 numberOfParams :: OpCode -> Int
 numberOfParams = \case
-  Add      -> 3
-  Multiply -> 3
-  Input    -> 1
-  Output   -> 1
-
--- | This opcode is voluntarily left out the 'OpCode' sum type because we want
--- to directly pattern match against its value instead of passing through
--- 'fromIntToOpCode' which would be inefficient
-haltCode :: Int
-haltCode = 99
+  Add         -> 3
+  Multiply    -> 3
+  Input       -> 1
+  Output      -> 1
+  JumpIfTrue  -> 2
+  JumpIfFalse -> 2
+  LessThan    -> 3
+  Equals      -> 3
+  Halt        -> 0
 
 data Mode = Position | Immediate
   deriving Show
@@ -62,72 +70,76 @@ parseFirstValue value = (opCode, fromIntToMode <$> [m1, m2, m3])
     -- returns the right-most digit of an integer and the remaining digits
     digits n = (mod n 10, div n 10)
 
-data Instruction =
-  Instruction
-  { opcode     :: OpCode
-  , parameters :: [(Mode, Int)]
-  } deriving Show
-
-data ReadingState =
-    Empty
-  | Reading OpCode [Mode] [Int] Int
-  -- ^      opcode, modes, params, number of params left to read
-
-splitProgram :: Program -> [Instruction]
-splitProgram program = snd . foldl' go (Empty, []) . takeWhile (/= haltCode) $ program
-  where
-    go (state :: ReadingState, instructions) n = case state of
-      Empty ->
-        let (opcode, modes) = parseFirstValue n
-            state' = Reading opcode modes [] (numberOfParams opcode)
-         in (state', instructions)
-      Reading opcode modes params 1 ->
-        let instruction = Instruction opcode $ zip modes (params ++ [n])
-         in (Empty, instructions ++ [instruction])
-      Reading opcode modes params count ->
-        let state' = Reading opcode modes (params ++ [n]) (count-1)
-         in (state', instructions)
-
 data ProgramIO =
   ProgramIO
-  { programState :: Program
-  , inputs  :: [Int]
+  { program :: Program
+  , pointer :: Int
+  , input   :: Int
   , outputs :: [Int]
-  }
+  } deriving Show
 
-runProgram ::
-     ProgramIO -- ^ the IntCode program with its inputs
-  -> ProgramIO -- ^ the final state of the Intcode program and the outputs
-runProgram programIO@(ProgramIO program0 _ _) =
-  foldl' runInstructionAndIO programIO instructions
-    where
-      instructions = splitProgram program0
-      runInstructionAndIO p@(ProgramIO program ins outs) instruction =
-        case runInstruction program (listToMaybe ins) instruction of
-          Pure program'      -> p{programState=program'}
-          ReadInput program' -> p{programState=program', inputs=tail ins}
-          ReturnsOutput program' output
-            -> p{programState=program', outputs = outs ++ [output]}
+runProgram, updateProgram :: ProgramIO -> ProgramIO
+runProgram p@ProgramIO{pointer}
+  | pointer < 0 = p
+  | otherwise   = runProgram $ updateProgram p
 
-data InstructionOutput =
-    Pure Program
-  | ReadInput Program
-  | ReturnsOutput Program Int
+updateProgram p@ProgramIO{..} = executeInstruction p opcode $ zip params modes
+  where
+    (opcode, modes) = parseFirstValue $ program !! pointer
+    numParams       = numberOfParams opcode
+    params          = [program !! (pointer + i) | i <- [1..numParams]]
 
-runInstruction ::
-     Program              -- ^ the initial program
-  -> Maybe Int            -- ^ maybe an input
-  -> Instruction          -- ^ the instruction
-  -> InstructionOutput    -- ^ the modified program and maybe an IO action
-runInstruction program mbInput (Instruction opcode params) = undefined
+executeInstruction :: ProgramIO -> OpCode -> [(Int, Mode)] -> ProgramIO
+executeInstruction p@ProgramIO{..} opcode ps = programIO'
+  where
+    p' = p{pointer = pointer + numberOfParams opcode + 1}
+    value (param, mode) = case mode of
+      Position  -> program !! param
+      Immediate -> param
+
+    programIO' = case opcode of
+      Add      -> p'{program = program & ix (fst $ ps !! 2) .~ value (ps !! 0) + value (ps !! 1)}
+      Multiply -> p'{program = program & ix (fst $ ps !! 2) .~ value (ps !! 0) * value (ps !! 1)}
+      Input    -> p'{program = program & ix (fst $ ps !! 0) .~ input}
+      Output   -> p'{outputs = outputs ++ [value $ ps !! 0]}
+      JumpIfTrue  -> if value (ps !! 0) /= 0 then p{pointer = value (ps !! 1)} else p'
+      JumpIfFalse -> if value (ps !! 0) == 0 then p{pointer = value (ps !! 1)} else p'
+      LessThan    -> if value (ps !! 0) < value (ps !! 1)
+                      then p'{program = program & ix (fst $ ps !! 2) .~ 1}
+                      else p'{program = program & ix (fst $ ps !! 2) .~ 0}
+      Equals      -> if value (ps !! 0) == value (ps !! 1)
+                      then p'{program = program & ix (fst $ ps !! 2) .~ 1}
+                      else p'{program = program & ix (fst $ ps !! 2) .~ 0}
+
+      Halt     -> p {pointer = -1} -- Halt returns the program unchanged
 
 part1 :: IO ()
 part1 = do
   program <- readInput
-  print program
+  let
+    programIO = ProgramIO program 0 1 []
+    result    = runProgram programIO
+  print result
 
 part2 :: IO ()
-part2 = undefined
+part2 = do
+  program <- readInput
+  putStrLn "Please enter input:"
+  input <- read <$> getLine
+  -- let
+  --   samplePrograms = (\p -> ProgramIO p 0 input []) <$> testPrograms
+  --   results    = runProgram <$> samplePrograms
+  let result = runProgram $ ProgramIO program 0 input []
+
+  print result
+
+testPrograms :: [Program]
+testPrograms = [equals8P, lessThan8P, equals8I, lessThan8I]
+  where
+    equals8P   = [3,9,8,9,10,9,4,9,99,-1,8]
+    lessThan8P = [3,9,7,9,10,9,4,9,99,-1,8]
+    equals8I   = [3,3,1108,-1,8,3,4,3,99]
+    lessThan8I = [3,3,1107,-1,8,3,4,3,99]
 
 readInput :: IO Program
-readInput = readProgram "day04/example1.csv"
+readInput = readProgram "day04/example2.csv"
